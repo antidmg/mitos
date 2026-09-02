7dff70fc708d9cf6e224e073e6291c91714ba5b0
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-09-02 17:30:42 +0200

    fix(ui): improve panel border contrast

--- crates/term/src/ui/picker.rs
+++ crates/term/src/ui/picker.rs
@@ -1006,7 +1006,7 @@ impl<T: 'static + Send + Sync, D: 'static + Send + Sync> Picker<T, D> {
         let background = cx.editor.theme.get("ui.background");
         surface.clear_with(area, background);
 
-        let block = panel::bordered(&cx.editor.theme);
+        let block = tui::panel::bordered(text_style);
         let inner = block.inner(area);
         block.render(area, surface);
 
@@ -1182,9 +1182,10 @@ impl<T: 'static + Send + Sync, D: 'static + Send + Sync> Picker<T, D> {
 
     fn render_preview(&mut self, area: Rect, surface: &mut Surface, cx: &mut Context) {
         let background = cx.editor.theme.get("ui.background");
+        let text_style = cx.editor.theme.get("ui.text");
         surface.clear_with(area, background);
 
-        let block = panel::horizontally_padded(&cx.editor.theme);
+        let block = tui::panel::horizontally_padded(text_style);
         let inner = block.inner(area);
         block.render(area, surface);
 
@@ -1364,7 +1365,7 @@ impl<I: 'static + Send + Sync, D: 'static + Send + Sync> Component for Picker<I,
         let render_preview =
             self.show_preview && self.file_fn.is_some() && area.width > MIN_AREA_WIDTH_FOR_PREVIEW;
         let (picker_area, _) = split_picker_area(area, render_preview);
-        let area = panel::bordered(&editor.theme)
+        let area = tui::panel::bordered(editor.theme.get("ui.text"))
             .inner(picker_area)
             .with_height(1)
             .clip_left(1);
